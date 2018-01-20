91e946585ebde97a77d282b1923d079a1410e7d0
Savastaro <guyfollypeke@gmail.com>
2018-01-20 18:17:47 +0100

    Ajout methode

---
+++ 24H/src/main/java/Fourmi/Feu.java
@@ -0,0 +1,14 @@
+package Fourmi;
+
+public class Feu {
+	
+	private boolean feu = false;
+
+	public boolean isFeu() {
+		return feu;
+	}
+
+	public void setFeu(boolean feu) {
+		this.feu = feu;
+	}
+}
--- 24H/src/main/java/Fourmi/Fourmis.java
+++ 24H/src/main/java/Fourmi/Fourmis.java
@@ -2,6 +2,104 @@ package Fourmi;
 
 public class Fourmis {
 	
+	private double vitesse;
+	private boolean sens;
+	private boolean support;
+	private Node position;
+	private boolean prioDroite;
+	public double getVitesse() {
+		return vitesse;
+	}
+
+
+	public void setVitesse(double vitesse) {
+		this.vitesse = vitesse;
+	}
+
+
+	public Node getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(Node position) {
+		this.position = position;
+	}
+
+
+	public boolean isPrioDroite() {
+		return prioDroite;
+	}
+
+
+	public void setPrioDroite(boolean prioDroite) {
+		this.prioDroite = prioDroite;
+	}
+
+
+	Feu feu = new Feu();
+	
+	public boolean isSens() {
+		return sens;
+	}
+
+
+	public void setSens(boolean sens) {
+		this.sens = sens;
+	}
+
+
+	public boolean isSupport() {
+		return support;
+	}
+
+
+	public void setSupport(boolean support) {
+		this.support = support;
+	}
+
+
+	public Feu getFeu() {
+		return feu;
+	}
+
+
+	public void setFeu(Feu feu) {
+		this.feu = feu;
+	}
+	
+	
+	public boolean verifStopFeu(){
+		
+		if(feu.isFeu() == true)
+		{
+			if (vitesse == 0)
+			{
+				return true;
+			}
+		}
+		
+		return false;
+			
+	};
+	
+	public boolean verifRalentPrioDroite() {
+		
+		if(prioDroite == true)
+		{
+			if (vitesse<=25 && vitesse>=10)
+			{
+				return true;
+			}
+				
+		}
+		return false;
+	}
+		
+		
+	
+
+	
 	
 	
 	
