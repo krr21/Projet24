f489d820c5688f8b932bea88b86b478efe05665b
IreneNoelle <ndele23@gmail.com>
2018-01-20 16:13:57 +0100

    Création des classes!!!

--- 24H/src/main/java/Fourmi/Fourmis.java
+++ 24H/src/main/java/Fourmi/Fourmis.java
@@ -1,4 +1,8 @@
+package Fourmi;
 
 public class Fourmis {
-
+	
+	
+	
+	
 }
---
+++ 24H/src/main/java/Fourmi/Graine.java
@@ -0,0 +1,24 @@
+package Fourmi;
+
+public class Graine {
+	
+	
+	public String lat;
+	public String lon;
+	
+	public Graine(String latitude,String longitude) {
+		latitude = lat;
+		longitude = lon;
+	}
+	
+	public void deposerGraine() {
+		
+	}
+	
+	public void supprimerGraine() {
+		
+	}
+	
+	
+	
+}
