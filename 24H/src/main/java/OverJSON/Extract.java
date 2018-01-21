3c497b51765c978470ea8f8dff5ca6febaf9486d
Unknown <mgrkrr21@gmail.com>
2018-01-21 02:40:36 +0100

    overp

---
+++ 24H/src/main/java/OverJSON/Extract.java
@@ -0,0 +1,43 @@
+package OverJSON;
+
+import java.util.List;
+
+public class Extract {
+
+	private String type;
+	private Long id;
+	private double lat;
+	private double lon;
+	private List<Long>nodes;
+	
+	
+	public List<Long> getNodes() {
+		return nodes;
+	}
+	public void setNodes(List<Long> nodes) {
+		this.nodes = nodes;
+	} String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLon() {
+		return lon;
+	}
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+}
